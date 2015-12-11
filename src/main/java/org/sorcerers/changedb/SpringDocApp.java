2a4834abf4c2004182120ad7bd893e3d2013061f
Marco Vit <mvit1@tudelft.nl>
2015-12-11 01:06:55 +0100

    merged two very similar main

--- src/main/java/org/sorcerers/changedb/SpringDocApp.java
+++ src/main/java/org/sorcerers/changedb/SpringDocApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpadocApp {
+public class SpringDocApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpadocApp.class, args);
+		SpringApplication.run(SpringDocApp.class, args);
     }
 }
